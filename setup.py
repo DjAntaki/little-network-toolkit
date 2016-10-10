b311ea1b938605b73601f2ab90686a005417a5f4
antaki.vincent@gmail.com <antaki.vincent@gmail.com>
2016-10-10 14:10:36 -0400

    update

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+#https://docs.python.org/2/distutils/setupscript.html
+
+setup(name="Little network toolkit",
+      version='0.',
+      author="Vincent Antaki",
+      author_email="vincent.antaki@mail.mcgill.ca",
+      url="",
+      packages=["networkx",'python-community','images2gif']
+      )
+
+#optionnal_packages = ['nose','sphinx','coverage.py']
