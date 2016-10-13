365e82942314d83b08e9470f0bf4c65f83985524
DjAntaki <antaki.vincent@gmail.com>
2016-10-13 11:23:15 -0400

    added travis CI build info

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
-
+#from distutils.core import setup
+from setuptools import setup
 #https://docs.python.org/2/distutils/setupscript.html
 
-setup(name="Little network toolkit",
-      version='0.',
+setup(name="LNTk",
+      version='0.1',
       author="Vincent Antaki",
       author_email="vincent.antaki@mail.mcgill.ca",
       url="",
-      packages=["networkx",'python-community','images2gif']
+#      packages=["networkx",'python-community','images2gif']
+      install_requires=["networkx",'selenium','xvfbwrapper']
       )
 
 #optionnal_packages = ['nose','sphinx','coverage.py']
